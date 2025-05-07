4f50c772e074c608b5bedc57de01ce294981c667
Jonathan Ardenoy <jonathan@coloweb.be>
2025-05-08 00:06:28 +0900

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _04_Fun_with_2D_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
